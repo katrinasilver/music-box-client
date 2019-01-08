bd85a8e5cf25cb55e75e4d9157c373c91d5507cd
Katrina Agustin <katrina.agustin@gmail.com>
2019-01-08 13:35:48 -0800

    added redux-thunk

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import artists from './reducers'
 import logger from 'redux-logger'
+import thunk from 'redux-thunk'
 const reducers = combineReducers({ artists })
-export default createStore(reducers, applyMiddleware(logger))
+export default createStore(reducers, applyMiddleware(logger, thunk))
